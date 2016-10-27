8e910ebf83726b2d59d1c1f7557723769e74440d
Ian Wait <ianlwait@gmail.com>
2016-10-27 12:00:25 -0400

    xD

---
+++ app/src/main/java/com/example/yea55/geoquiz/Question.java
@@ -0,0 +1,14 @@
+package com.example.yea55.geoquiz;
+
+/**
+ * Created by 236808 on 10/27/2016.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
